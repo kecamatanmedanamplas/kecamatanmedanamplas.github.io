6dd8eeefed80d71963c08ba05eeeb93ba9d34dc6
kecamatanmedanamplas <160447322+kecamatanmedanamplas@users.noreply.github.com>
2024-02-19 11:19:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Sistem Informasi Pelayanan Publik</title>
+   <link rel="shorcut icon" href="user/img/icon.png">
+   <link href="aset/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+   
+   <div class="carousel">
+      <a href="user/index.php">
+      <div class="carousel_inner">
+         <div class="carousel_item carousel_item__active">
+            <img src="user/img/slide.jpeg" alt="" class="carousel_img d-block vw-100 vh-100" style="ima">
+         </div>
+         <div class="carousel_item">
+            <img src="user/img/slide.jpg" alt="" class="carousel_img d-block vw-100 vh-100">
+         </div>
+      </div>
+
+      <div class="carousel_indicator">
+         <button class="carousel_dot carousel_dot__active"></button>
+         <button class="carousel_dot"></button>
+         <button class="carousel_dot"></button>
+      </div>
+      
+      <div class="carousel_control">
+         <button class="carousel_button carousel_button__prev">
+            <i class="fas fa-chevron-left"></i>
+         </button>
+         <button class="carousel_button carousel_button__next">
+            <i class="fas fa-chevron-right"></i>
+         </button>
+      </div>
+   </a>  
+   </div>
+
+   <script src="script.js"></script>
+</body>
+
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Sistem Informasi Pelayanan Publik</title>
+   <link rel="shorcut icon" href="user/img/icon.png">
+   <link href="aset/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+   
+   <div class="carousel">
+      <a href="user/index.php">
+      <div class="carousel_inner">
+         <div class="carousel_item carousel_item__active">
+            <img src="user/img/slide.jpeg" alt="" class="carousel_img d-block vw-100 vh-100" style="ima">
+         </div>
+         <div class="carousel_item">
+            <img src="user/img/slide.jpg" alt="" class="carousel_img d-block vw-100 vh-100">
+         </div>
+      </div>
+
+      <div class="carousel_indicator">
+         <button class="carousel_dot carousel_dot__active"></button>
+         <button class="carousel_dot"></button>
+         <button class="carousel_dot"></button>
+      </div>
+      
+      <div class="carousel_control">
+         <button class="carousel_button carousel_button__prev">
+            <i class="fas fa-chevron-left"></i>
+         </button>
+         <button class="carousel_button carousel_button__next">
+            <i class="fas fa-chevron-right"></i>
+         </button>
+      </div>
+   </a>  
+   </div>
+
+   <script src="script.js"></script>
+</body>
+
+>>>>>>> ec822170fc3387597a0b0c6d889a3cdc6641bc7a
+</html>
